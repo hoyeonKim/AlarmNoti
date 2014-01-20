8e3e15e7beaffcb9b172bbf8e37d8449c9b399bb
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-20 11:09:46 +0900

    Initial Commit

---
+++ 3p11/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  3p11
+//
+//  Created by SDT-1 on 2014. 1. 20..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
